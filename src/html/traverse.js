b6f2a7c2d8cbcce278d3ff47c8231461957f9f52
Tomer Aberbach <tomeraberbach@gmail.com>
2020-09-12 18:58:17 -0400

    feat: implement jsx to html string renderer

---
+++ src/html/traverse.js
@@ -0,0 +1,17 @@
+import { isElement } from './index'
+
+const traverse = (element, fn) => {
+  const stack = [element]
+  do {
+    const current = stack.pop()
+    fn(current)
+
+    for (const child of current.children) {
+      if (isElement(child)) {
+        stack.push(child)
+      }
+    }
+  } while (stack.length > 0)
+}
+
+export default traverse
---
+++ src/html/traverse.test.js
@@ -0,0 +1,23 @@
+import test from 'ava'
+import traverse from './traverse'
+
+test(`traverse concrete example`, t => {
+  let count = 0
+
+  traverse(
+    <body>
+      <h1>Hello World!</h1>
+      <article>
+        <h2>The best article</h2>
+        <p>
+          In the
+          {` `}
+          <b>beginning</b>, there was HTML!
+        </p>
+      </article>
+    </body>,
+    () => count++
+  )
+
+  t.is(count, 6)
+})
