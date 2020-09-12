2a850e1455da89393b2c744714de8343b31b7a52
Tomer Aberbach <tomeraberbach@gmail.com>
2020-09-12 18:57:00 -0400

    feat: implement jsx pragma that returns vdom

---
+++ src/html/arbitraries.js
@@ -0,0 +1,59 @@
+import { fc } from 'ava-fast-check'
+import { html } from './index'
+import { getNonSelfClosingTags, getSelfClosingTags, getTags } from './tags'
+
+const constantArb = arb => () => arb
+
+export const tagArb = constantArb(fc.constantFrom(...getTags()))
+
+export const selfClosingTagArb = constantArb(
+  fc.constantFrom(...getSelfClosingTags())
+)
+
+export const nonSelfClosingTagArb = constantArb(
+  fc.constantFrom(...getNonSelfClosingTags())
+)
+
+export const attributeNameArb = fc.string
+
+export const attributeValueArb = () =>
+  fc.oneof(fc.string(), fc.integer(), fc.float(), fc.boolean())
+
+export const attributeArb = () =>
+  fc.tuple(attributeNameArb(), attributeValueArb())
+
+export const attributesArb = () =>
+  fc.dictionary(attributeNameArb(), attributeValueArb())
+
+export const nonElementChildArb = () =>
+  fc.oneof(fc.string(), fc.integer(), fc.float())
+
+export const selfClosingElementArb = () =>
+  fc
+    .tuple(selfClosingTagArb(), attributesArb())
+    .map(([tag, attributes]) => html(tag, attributes))
+
+export const nonSelfClosingElementArb = fc.memo(n =>
+  fc
+    .tuple(
+      nonSelfClosingTagArb(),
+      attributesArb(),
+      // eslint-disable-next-line no-use-before-define
+      childrenArb(n)
+    )
+    .map(([tag, attributes, children]) => html(tag, attributes, ...children))
+)
+
+export const elementArb = fc.memo(n =>
+  fc.oneof(selfClosingElementArb(), nonSelfClosingElementArb(n))
+)
+
+export const childArb = fc.memo(n =>
+  fc.oneof(elementArb(n), elementArb(n), nonElementChildArb())
+)
+
+export const childrenArb = fc.memo(n =>
+  fc.array(n <= 1 ? nonElementChildArb() : childArb(n - 1))
+)
+
+export const componentArb = fc.memo(n => fc.func(elementArb(n)))
---
+++ src/html/index.js
@@ -0,0 +1,39 @@
+import assert from 'assert'
+import { isSelfClosingTag, isTag } from './tags'
+
+const elements = new WeakSet()
+
+export const isElement = value => elements.has(value)
+
+export const html = (tag, attributes, ...children) => {
+  attributes = attributes ?? {}
+
+  if (typeof tag === `function`) {
+    return tag({ ...attributes, children })
+  }
+
+  assert(isTag(tag))
+  assert(children.length === 0 || !isSelfClosingTag(tag))
+
+  for (const [name, value] of Object.entries(attributes)) {
+    assert(typeof name === `string`)
+    assert(
+      value == null || [`string`, `number`, `boolean`].includes(typeof value)
+    )
+  }
+
+  for (const child of children) {
+    for (const grandchild of [].concat(child)) {
+      assert(
+        elements.has(grandchild) ||
+          grandchild == null ||
+          [`string`, `number`].includes(typeof grandchild)
+      )
+    }
+  }
+
+  const element = { tag, attributes, children }
+  elements.add(element)
+
+  return element
+}
