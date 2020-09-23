d2f6d2ae1cc74848255c456b3d6ed546d538335d
Tomer Aberbach <tomeraberbach@gmail.com>
2020-09-23 18:38:00 -0400

    refactor: access tag sets directly

--- src/html/tags.js
+++ src/html/tags.js
@@ -2,21 +2,11 @@ import htmlTags from 'html-tags'
 import svgTagNames from 'svg-tag-names'
 import voidHtmlTags from 'html-tags/void'
 
-const createSetFunctions = set => ({
-  get: () => new Set(set),
-  is: value => set.has(value)
-})
+export const tags = new Set(htmlTags.concat(svgTagNames))
 
-const tags = new Set(htmlTags.concat(svgTagNames))
-export const { get: getTags, is: isTag } = createSetFunctions(tags)
+export const selfClosingTags = new Set(voidHtmlTags)
 
-const selfClosingTags = new Set(voidHtmlTags)
-export const {
-  get: getSelfClosingTags,
-  is: isSelfClosingTag
-} = createSetFunctions(selfClosingTags)
-
-const nonSelfClosingTags = new Set(
+export const nonSelfClosingTags = new Set(
   (function* () {
     for (const tag of tags) {
       if (!selfClosingTags.has(tag)) {
@@ -25,7 +15,3 @@ const nonSelfClosingTags = new Set(
     }
   })()
 )
-export const {
-  get: getNonSelfClosingTags,
-  is: isNonSelfClosingTag
-} = createSetFunctions(nonSelfClosingTags)
