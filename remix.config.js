c76ed29b36c5eff9bb67d8d9b0eb9dbea3f61f0c
Tomer Aberbach <tomeraberbach@gmail.com>
2022-10-02 22:42:06 -0400

    fix: remix config

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   appDirectory: `src`,
   assetsBuildDirectory: `public/build`,
-  publicPath: `build`,
+  publicPath: `/build`,
   serverBuildPath: `build/index.js`,
+  // eslint-disable-next-line prefer-named-capture-group
+  serverDependenciesToBundle: [/^(?!(shiki|uglify-js)$).*/u],
   ignoredRouteFiles: [`**/.*`],
 }
