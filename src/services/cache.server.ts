c879652cbb2c91b3a5de691506106bcb84c301b4
Tomer Aberbach <tomeraberbach@gmail.com>
2022-12-11 00:54:01 -0500

    refactor: use mem for caching (#25)

--- src/services/cache.server.ts
+++ src/services/cache.server.ts
@@ -1,40 +1,14 @@
 import { nextTick } from 'process'
+import mem from 'mem'
 
 export const cached = <Value>(fn: () => Value): (() => Value) => {
   if (process.env.NODE_ENV !== `production`) {
     return fn
   }
 
+  const cachedFn = mem(fn)
+
   // Call and cache asynchronously to ensure dependencies are initialized
-  nextTick(() => get(fn))
-
-  return () => get(fn)
+  nextTick(cachedFn)
+  return cachedFn
 }
-
-export const refreshCache = (): void => {
-  for (const entry of cache.values()) {
-    entry.expired = true
-  }
-
-  for (const fn of cache.keys()) {
-    get(fn)
-  }
-}
-
-const get = <Value>(fn: () => Value): Value => {
-  let entry = cache.get(fn)
-
-  if (!entry) {
-    cache.set(fn, (entry = { value: undefined, expired: true }))
-  }
-
-  if (entry.expired) {
-    entry.value = fn()
-    entry.expired = false
-  }
-
-  return entry.value as Value
-}
-
-const cache: Map<() => unknown, { value: unknown; expired: boolean }> =
-  new Map()
