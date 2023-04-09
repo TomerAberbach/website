7dc28c4dd4e6bcaef690e7bbfab036914cac71c6
Tomer Aberbach <tomeraberbach@gmail.com>
2023-04-09 16:18:48 -0400

    chore: give up on cypress

--- cypress/e2e/index.cy.ts
+++
@@ -1,5 +0,0 @@
-it(`testing`, () => {
-  cy.visit(`/`, { headers: { 'Accept-Encoding': `gzip, deflate` } })
-
-  expect(true).to.equal(true)
-})
--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,10 +1,4 @@
 import { RemixBrowser } from '@remix-run/react'
 import { hydrateRoot } from 'react-dom/client'
-import { hydrate } from 'react-dom'
 
-// https://github.com/facebook/react/issues/24430#issuecomment-1162415060
-if (process.env.NODE_ENV === `test`) {
-  hydrate(<RemixBrowser />, document)
-} else {
-  hydrateRoot(document, <RemixBrowser />)
-}
+hydrateRoot(document, <RemixBrowser />)
