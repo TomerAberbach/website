ccc188f666cab544d292b699f78eca60d08b7cde
Tomer Aberbach <tomeraberbach@gmail.com>
2023-05-26 15:54:51 -0400

    style: format

--- src/routes/mano-simulator.ts
+++ src/routes/mano-simulator.ts
@@ -2,6 +2,4 @@ import type { LoaderFunction } from '@remix-run/server-runtime'
 import { redirect } from '@remix-run/server-runtime'
 
 export const loader: LoaderFunction = () =>
-  redirect(`https://tomeraberbach.github.io/mano-simulator`, {
-    status: 301,
-  })
+  redirect(`https://tomeraberbach.github.io/mano-simulator`, { status: 301 })
