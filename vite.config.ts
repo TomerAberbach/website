be5779b38c0117b8f959cec8705cb96b4495d324
Tomer Aberbach <tomeraberbach@gmail.com>
2024-06-23 18:57:44 -0400

    feat: use restart plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,56 @@
 import { vitePlugin as remix } from '@remix-run/dev'
-import { defineConfig } from 'vite'
+import { createLogger, defineConfig } from 'vite'
 import { installGlobals } from '@remix-run/node'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import imagemin from '@vheemstra/vite-plugin-imagemin'
+import imageminJpeg from 'imagemin-mozjpeg'
+import imageminGif from 'imagemin-gifsicle'
+import imageminWebp from 'imagemin-webp'
+import imageminPng from 'imagemin-pngquant'
+import imageminSvg from 'imagemin-svgo'
+import restart from 'vite-plugin-restart'
 
 installGlobals()
 
+const logger = createLogger()
+const originalWarning = logger.warn.bind(logger)
+logger.warn = (msg, options) => {
+  if (msg.includes(`[vite:css]`)) {
+    // TODO: Create fallback fonts for KaTeX fonts.
+    return
+  }
+
+  if (
+    msg.includes(`[plugin:vite:resolve]`) &&
+    msg.includes(`externalized for browser compatibility`)
+  ) {
+    // Importing server code in routes is the standard Remix practice.
+    return
+  }
+
+  originalWarning(msg, options)
+}
+
 export default defineConfig({
+  customLogger: logger,
   server: { port: 3000 },
   plugins: [
     remix({ appDirectory: `src`, ignoredRouteFiles: [`**/.*`] }),
     tsconfigPaths(),
-    {
-      name: `restart-for-markdown-change`,
-      handleHotUpdate: async ({ server, file }) => {
-        if (file.endsWith(`.md`)) {
-          await server.restart()
-        }
-        return []
+    imagemin({
+      plugins: {
+        gif: imageminGif(),
+        jpg: imageminJpeg(),
+        png: imageminPng(),
+        svg: imageminSvg(),
       },
-    },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+    restart({ restart: [`private/posts/*.md`, `**/*.server.tsx?`] }),
   ],
 })
