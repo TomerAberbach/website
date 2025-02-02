8a90e1e7cc2e04f3f69d94aff3bf6d8899ea61c2
Tomer Aberbach <tomer@aberba.ch>
2025-02-02 16:16:02 -0500

    fix: building

--- scripts/build.ts
+++ scripts/build.ts
@@ -23,7 +23,10 @@ const fontsBuildPath = fromRoot(`private/fonts/build`)
 await $`mkdir -p ${fontsBuildPath}`
 const runServerCommand = `react-router-serve ${fromRoot(`build/server/index.js`)}`
 const glyphhangerCommand = `glyphhanger http://localhost:3000 --spider-limit=0 --formats=woff,woff2 --subset=${fromRoot(`private/fonts/*.ttf`)} --output=${fontsBuildPath}`
-await $`start-server-and-test ${runServerCommand} http-get://localhost:3000 ${glyphhangerCommand}`
+await $({
+  // `get-stdin` hangs inside `glyphhanger` without this.
+  stdio: 'inherit',
+})`start-server-and-test ${runServerCommand} http-get://localhost:3000 ${glyphhangerCommand}`
 
 // Rebuild with new fonts and minify
 await $`react-router build`
